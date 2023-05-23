20ed0f30a854ae99d2b4a20137ed2508d51b8d32
alnepravskaya@gmail.com <alnepravskaya@gmail.com>
2023-05-23 12:07:34 +0200

    update docker file

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+app.use(cors())
+app.options('*', cors())
 
-console.log('server is up!');
+console.log('server is up! 2');
 
 import { router as todoListRoutes } from '../routers/todoCategories';
 import { router as todoItemsRoutes } from '../routers/todoItems';
